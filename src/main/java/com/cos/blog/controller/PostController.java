e385a344f285b5be63447e0e7277dcb0cafc1036
GZM <gimzimean@gmail.com>
2020-02-16 16:46:54 +0900

    show up lists

--- src/main/java/com/cos/blog/controller/PostController.java
+++ src/main/java/com/cos/blog/controller/PostController.java
@@ -1,16 +1,24 @@
 package com.cos.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.cos.blog.service.PostService;
+
 @Controller
 public class PostController {
+	@Autowired
+	private PostService postService;
 
 	@GetMapping({"","/","/post"})
-	public String posts() {
+	public String posts(Model model) {
 		//view resolver가 관여해야하기 떄문에 @responsebody ㄴ ㄴ 
+		
+
+		model.addAttribute("Post", postService.목록보기());
 		return"/post/list";
 	}
 	
@@ -29,4 +37,5 @@ public class PostController {
 	public String update() {
 		return "/post/update";
 	}
+	
 }
--- src/main/java/com/cos/blog/service/PostService.java
+++ src/main/java/com/cos/blog/service/PostService.java
@@ -1,8 +1,21 @@
 package com.cos.blog.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cos.blog.model.post.dto.ResponseListDto;
+import com.cos.blog.repository.PostRepository;
 
 @Service
 public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<ResponseListDto> 목록보기() {
+		return postRepository.findAll();
+	}
 
 }
